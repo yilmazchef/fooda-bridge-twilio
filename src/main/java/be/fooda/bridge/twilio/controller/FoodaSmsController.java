8af3317b92c173972b096c673bef09b09d66c1fd
yilmazchef <yilmaz@mail.be>
2021-01-29 23:15:37 +0100

    phone number regex is corrected.

--- src/main/java/be/fooda/bridge/twilio/controller/FoodaSmsController.java
+++ src/main/java/be/fooda/bridge/twilio/controller/FoodaSmsController.java
@@ -36,10 +36,11 @@ public class FoodaSmsController {
     @PostMapping("/send")
     public ResponseEntity sendMessages(@RequestBody MessageRequest messageRequest) {
 
-        String regex = "^\\+[0-9]{1,3}\\.[0-9]{4,14}(?:x.+)?$";
+        String regex = "\\+(9[976]\\d|8[987530]\\d|6[987]\\d|5[90]\\d|42\\d|3[875]\\d|2[98654321]\\d|9[8543210]|8[6421]|6[6543210]|5[87654321]|4[987654310]|3[9643210]|2[70]|7|1)\\d{1,14}$\n";
         Pattern pattern = Pattern.compile(regex);
 
         for (String number : messageRequest.getNumbers()) {
+            if (number.startsWith("+")) number = number.replace("+", "00");
             Matcher matcher = pattern.matcher(number);
 
             if (matcher.matches()) {
